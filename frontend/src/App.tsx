1db8fb8e955591eb8b7d8f4cf2b3ea755f0bed19
hanz <hi@hanz.dev>
2026-04-07 08:31:53 +0700

    feat: add frontend src files main app scss css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Vite + React + SCSS</h1>
+        <p>Frontend scaffold ready for development</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
