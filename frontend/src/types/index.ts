5a978ac1639a0a7ceab784c761fd9e53e73b10d9
hanz <hi@hanz.dev>
2026-04-07 08:50:15 +0700

    feat: add TypeScript types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,27 @@
+export interface MenuItem {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
+
+export interface Restaurant {
+  id: string;
+  name: string;
+  cuisine: string;
+  rating: number;
+  lat: number;
+  lng: number;
+  menu: MenuItem[];
+}
+
+export interface CartItem {
+  item: MenuItem;
+  quantity: number;
+  restaurantId: string;
+}
+
+export interface CartState {
+  items: CartItem[];
+  restaurantId: string | null;
+}
